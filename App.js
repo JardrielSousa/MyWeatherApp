cdd037e7119f5a818d578a92ef386529e2b35e0b
jardriel sousa <jardriel.redes18@gmail.com>
2021-08-25 00:25:55 -0300

    was implemented the method to find weather

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [city, setCity] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [temperatura, setTemperatura] = useState([]);
+
+  const getWeather = async () => {
+    try {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=Belo horizonte&APPID=4efbe3606dc68b3acd60263d58fa773f"
+      );
+      const json = await response.json();
+      setData(json.weather);
+      setCity(json.name);
+      setCountry(json.sys.country);
+      setTemperatura(json.main);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }, [lat, long]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.headerCard}>
+        {city},{country}
+      </Text>
+      <div
+        style={{
+          backgroundColor: "grey",
+          padding: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={styles.textColor}>
+          {Math.round(temperatura.temp) / 10} &deg;C
+        </Text>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text style={styles.textColor}>
+                {item.description}, {item.main}
+              </Text>
+            )}
+          />
+        )}
+      </div>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  textColor: {
+    color: "white",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    width: 200,
+    height: 50,
+  },
+  headerCard: {
+    backgroundColor: "#40C2FF",
+    color: "white",
+    padding: 10,
+    borderRadius: 5,
+  },
+  bodyCard: {
+    backgroundColor: "grey",
+    padding: 10,
+    borderRadius: 5,
   },
 });
---
+++ config/config.js
@@ -0,0 +1,3 @@
+var weatherConfig = {
+  apiKey: "4efbe3606dc68b3acd60263d58fa773f",
+};
